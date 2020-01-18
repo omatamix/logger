1acb20a87e2f801b1c7f76959c414c2abdd35836
ezoheux <46251064+ezoheux@users.noreply.github.com>
2020-01-18 13:21:19 -0500

    Add a predefined function to the storage interface.

--- src/StorageInterface.php
+++ src/StorageInterface.php
@@ -7,4 +7,13 @@ namespace Zane\Logger;
  */
 interface StorageInterface
 {
+    /**
+     * Sends a log message to the storage.
+     *
+     * @param mixed  $level   The log level.
+     * @param string $message The log message.
+     *
+     * @return void Returns nothing.
+     */
+    public function log($level, $message): void;
 }
